90af3a263c5c3d9780a3ac43c8ba93eb60450a3c
Daniel de la Huerta <ddelahuerta@gmail.com>
2021-09-14 15:46:03 -0400

    bootstrap is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/Home';
+import Nav from './components/nav/Nav';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <h1>lol</h1>
+      <Button>This is a button</Button>
+      <h1>Hello World</h1>
+      <Home />
+  
     </div>
   );
 }
